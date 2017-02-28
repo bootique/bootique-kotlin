a076f4ce1f13d974e79fa7a9afbc91f17fb00f14
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-02-28 03:08:47 +0300

    More test cases.

--- bootique-kotlin/src/test/java/io/bootique/kotlin/ConfigurationTest.kt
+++ bootique-kotlin/src/test/java/io/bootique/kotlin/ConfigurationTest.kt
@@ -1,19 +1,51 @@
 package io.bootique.kotlin
 
 import io.bootique.kotlin.config.modules.config
-import org.junit.Assert.assertTrue
+import io.bootique.kotlin.config.modules.jetty
+import io.bootique.kotlin.config.modules.logback
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 internal data class Sample(val text: String)
 
 class ConfigurationTest {
 
-    @Test fun test() {
+    @Test fun singleConfig() {
         val config = config {
             addConfig("Yoda" to Sample("Jedi"))
         }
 
-        assertTrue(config["Yoda"] == Sample("Jedi"))
+        assertEquals(Sample("Jedi"), config["Yoda"])
     }
 
+    @Test fun multipleConfigs() {
+        val config = config {
+            addConfig("Yoda" to Sample("Jedi"))
+            addConfig("Vader" to Sample("Sith"))
+        }
+
+        assertEquals(Sample("Jedi"), config["Yoda"])
+        assertEquals(Sample("Sith"), config["Vader"])
+    }
+
+    @Test fun jettyConfig() {
+        val config = config {
+            jetty {
+
+            }
+        }
+
+        assertNotNull(config["jetty"])
+    }
+
+    @Test fun logbackConfig() {
+        val config = config {
+            logback {
+
+            }
+        }
+
+        assertNotNull(config["log"])
+    }
 }
