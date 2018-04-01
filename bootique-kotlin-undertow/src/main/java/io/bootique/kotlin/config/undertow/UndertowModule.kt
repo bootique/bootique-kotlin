72c5c6317e5dacd003399a111fa5f926de38be3a
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-04-02 00:48:25 +0300

    Delete unused config properties.

--- bootique-kotlin-undertow/src/main/java/io/bootique/kotlin/config/undertow/UndertowModule.kt
+++ bootique-kotlin-undertow/src/main/java/io/bootique/kotlin/config/undertow/UndertowModule.kt
@@ -1,7 +1,5 @@
 package io.bootique.kotlin.config.undertow
 
-import io.bootique.resource.FolderResourceFactory
-import io.bootique.undertow.StaticResourceFactory
 import io.bootique.undertow.UndertowFactory
 import io.bootique.undertow.config.HttpListener
 import io.bootique.undertow.config.HttpsListener
@@ -15,7 +13,6 @@ import io.bootique.undertow.config.HttpsListener
 fun undertowFactory(
     httpListeners: List<HttpListener> = listOf(),
     httpsListeners: List<HttpsListener> = listOf(),
-    staticFiles: List<StaticResourceFactory> = listOf(),
     bufferSize: Int? = null,
     ioThreads: Int? = null,
     workerThreads: Int? = null,
@@ -23,7 +20,6 @@ fun undertowFactory(
 ) = UndertowFactory().also {
     it.httpListeners = httpListeners
     it.httpsListeners = httpsListeners
-    it.staticFiles = staticFiles
     it.bufferSize = bufferSize
     it.ioThreads = ioThreads
     it.workerThreads = workerThreads
@@ -36,14 +32,4 @@ fun httpListener(
 ) = HttpListener().also {
     it.port = port
     it.host = host
-}
-
-fun staticResource(
-    path: FolderResourceFactory,
-    url: String,
-    isCache: Boolean = false
-) = StaticResourceFactory().also {
-    it.path = path
-    it.url = url
-    it.isCache = isCache
 }
--- bootique-kotlin-undertow/src/test/java/io/bootique/kotlin/config/undertow/UndertowModuleTest.kt
+++ bootique-kotlin-undertow/src/test/java/io/bootique/kotlin/config/undertow/UndertowModuleTest.kt
@@ -1,6 +1,5 @@
 package io.bootique.kotlin.config.undertow
 
-import io.bootique.resource.FolderResourceFactory
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -16,18 +15,11 @@ class UndertowModuleTest {
             httpListeners = listOf(
                 httpListener(8080)
             ),
-            staticFiles = listOf(
-                staticResource(
-                    path = FolderResourceFactory("/root"),
-                    url = "/test"
-                )
-            ),
             directBuffers = true
         )
 
         assertEquals(8080, config.httpListeners[0].port)
         assertEquals("0.0.0.0", config.httpListeners[0].host)
-        assertEquals("/test", config.staticFiles[0].url)
         assertEquals(true, config.directBuffers)
     }
 }
